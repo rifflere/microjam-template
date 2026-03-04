2772a148391b7d4911eeffc6730b28bde1e4da5e
rifflere <82406585+rifflere@users.noreply.github.com>
2026-03-04 19:15:27 +0000

    Added assets for solar eclipse game, added basic game structure with two sprites on screen

---
+++ include/reb/reb_eclipse_game.h
@@ -0,0 +1,49 @@
+#ifndef REB_ECLIPSE_GAME_H
+#define REB_ECLIPSE_GAME_H
+
+#include "bn_sprite_ptr.h"
+#include "mj/mj_game.h"
+#include "bn_sprite_items_reb_sun.h"
+#include "bn_sprite_items_reb_moon.h"
+#include "bn_sprite_items_reb_earth.h"
+#include "bn_sprite_animate_actions.h"
+#include "bn_optional.h"
+
+namespace reb
+{
+
+    class reb_eclipse_game : public mj::game
+    {
+        public:
+
+            static constexpr int minimum_frames = 3 * 60; 
+            static constexpr int maximum_frames = 5 * 60; 
+
+            reb_eclipse_game(int completed_games, const mj::game_data& data);
+
+            [[nodiscard]] bn::string<16> title() const final
+            {
+                return "Solar Eclipse";
+            }
+
+            [[nodiscard]] int total_frames() const final
+            {
+                return minimum_frames;
+            }
+
+            void fade_in(const mj::game_data& data) final;
+
+            [[nodiscard]] mj::game_result play(const mj::game_data& data) final;
+
+            [[nodiscard]] bool victory() const final;
+
+            void fade_out(const mj::game_data& data) final;
+
+        private:
+            bn::sprite_ptr _sunSprite = bn::sprite_items::reb_sun.create_sprite(-105,0);
+            bn::sprite_ptr _earthSprite = bn::sprite_items::reb_earth.create_sprite(75,0);
+            bool _victory;
+    };
+}
+
+#endif
---
+++ src/reb/reb_eclipse_game.cpp
@@ -0,0 +1,52 @@
+#include "reb_eclipse_game.h"
+
+#include "bn_keypad.h"
+#include "bn_sprite_ptr.h"
+
+#include "mj/mj_game_list.h"
+
+namespace
+{
+    constexpr bn::string_view code_credits[] = { "Rebecca Riffle" };
+    constexpr bn::string_view graphics_credits[] = { "" };
+    constexpr bn::string_view sfx_credits[] = {""};
+}
+
+MJ_GAME_LIST_ADD(reb::reb_eclipse_game)
+MJ_GAME_LIST_ADD_CODE_CREDITS(code_credits)
+MJ_GAME_LIST_ADD_GRAPHICS_CREDITS(graphics_credits)
+// MJ_GAME_LIST_ADD_MUSIC_CREDITS(music_credits)
+MJ_GAME_LIST_ADD_SFX_CREDITS(sfx_credits)
+
+namespace reb
+{
+    reb_eclipse_game::reb_eclipse_game([[maybe_unused]] int completed_games, [[maybe_unused]] const mj::game_data& data) :
+        mj::game("reb")
+    {
+    }
+
+    void reb_eclipse_game::fade_in([[maybe_unused]] const mj::game_data& data)
+    {
+        _sunSprite.set_position(-100, 0);
+        _earthSprite.set_position(75, 0);
+    }
+
+    mj::game_result reb_eclipse_game::play([[maybe_unused]] const mj::game_data& data)
+    {
+        _victory = false;
+
+        mj::game_result result(victory(), false);
+
+        return result;
+    }
+
+    bool reb_eclipse_game::victory() const
+    {
+        return _victory;
+    }
+
+    void reb_eclipse_game::fade_out([[maybe_unused]] const mj::game_data& data)
+    {
+    }
+
+}
