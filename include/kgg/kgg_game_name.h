f86318143a68f1dc2f508829ad9656f0d7a497b6
Gurpinder Gill <gill.gurpinder@student.greenriver.edu>
2026-03-12 10:42:42 -0700

    added speed of rocks and speed variable

--- include/kgg/kgg_game_name.h
+++ include/kgg/kgg_game_name.h
@@ -6,37 +6,36 @@
 #include "kgg/kgg_rock.h"
 #include "kgg/kgg_player.h"
 
-namespace kgg {
-
-class kgg_game_name : public mj::game
+namespace kgg
 {
 
-public:
+    class kgg_game_name : public mj::game
+    {
 
-    kgg_game_name(int completed_games, const mj::game_data& data);
+    public:
+        kgg_game_name(int completed_games, const mj::game_data &data);
 
-    bn::string<16> title() const override;
+        bn::string<16> title() const override;
 
-    int total_frames() const override;
+        int total_frames() const override;
 
-    mj::game_result play(const mj::game_data& data) override;
+        mj::game_result play(const mj::game_data &data) override;
 
-    bool victory() const override;
+        bool victory() const override;
 
-    void fade_in(const mj::game_data& data) override;
+        void fade_in(const mj::game_data &data) override;
 
-    void fade_out(const mj::game_data& data) override;
+        void fade_out(const mj::game_data &data) override;
 
-private:
-    bool _victory = false;
-    player _player;
-    bool _game_over = false; // helps stop the game once player gets hit
-    int _spawn_timer = 0; // helps deciding when to create a new rock
-    bn::vector<rock, 16> _rocks; // this can store up to 16 rocks
-    bn::fixed _rock_speed{}; // rock speed variable
-    bn::fixed _recommended_rock_speed(mj::difficulty_level difficulty); // current speed of rocks
-
-};
+    private:
+        bool _victory = false;
+        player _player;
+        bool _game_over = false;                                            // helps stop the game once player gets hit
+        int _spawn_timer = 0;                                               // helps deciding when to create a new rock
+        bn::vector<rock, 16> _rocks;                                        // this can store up to 16 rocks
+        bn::fixed _rock_speed{};                                            // rock speed variable
+        bn::fixed _recommended_rock_speed(mj::difficulty_level difficulty); // current speed of rocks
+    };
 
 }
 
